4f9ef50839600417e3b6809afa2aae7dc8bbb392
sofia <123276314+xXsofiax@users.noreply.github.com>
2023-02-04 15:26:49 -0800

    Created Javascript skeleton
    2/4/2023

---
+++ 2350 Assignment 1 Javascript.js
@@ -0,0 +1,17 @@
+let totalpop;
+let numOfreps = document.getElementById("").value;
+let avgofreps;
+
+function readCSV(){
+//read excel file
+}
+
+function calculateReps(){
+totalPop;
+avgOfreps = totalPop / avgOfreps;
+}
+            
+function displayOutput(){
+
+//display the states + number of representatives calculated
+}
