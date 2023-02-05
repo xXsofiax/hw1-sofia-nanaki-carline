360dad47bd4aa49e8f0f578db930a87777caf770
CarlineG <123274787+CarlineG@users.noreply.github.com>
2023-02-04 16:51:04 -0800

    Upload JavaScript file from Carline
    02-04-2023

---
+++ 2350 Assignment 1 JavaScript_Carline.js
@@ -0,0 +1,22 @@
+"use strict";
+let population = new Array(2);
+let totalpop;
+let averOfreps;
+function readCSV(){
+    //read the excel file and sign the data in population array.
+}
+
+function totalPo(){
+    //to add all the population together
+    for(let i = 0; i<population[1].length;i++){
+        totalpop += population[1][i];
+    }
+    return total;
+}
+
+function calculateReps(){
+    // to calculate the average of population
+    averOfreps = totalpop / population[1].length;
+    return averOfreps;
+}
+
