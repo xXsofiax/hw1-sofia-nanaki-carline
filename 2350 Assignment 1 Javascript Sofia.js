fb9dcb88f85e1874095e810dfec1319e9e34219f
sofia <123276314+xXsofiax@users.noreply.github.com>
2023-02-04 16:47:32 -0800

    Updated Javascript - sofia
    2/4/2023

---
+++ 2350 Assignment 1 Javascript Sofia.js
@@ -0,0 +1,27 @@
+let totalpop;
+let numOfreps; // default is 435
+let numOfrepsInput = document.getElementById("").value; // changes the number of reps if the user wants to :)
+let avgofreps;
+
+function readCSV(){
+
+    //read excel file
+}
+
+function calculateReps(){
+
+    totalPop;
+
+    //changes the number of reps if user inputs a number
+    if (numOfRepsInput >= 0){
+    }else
+
+    avgOfreps = totalPop / avgOfreps;
+    
+}
+            
+function displayOutput(){
+
+    //display the states + number of representatives calculated
+
+}
